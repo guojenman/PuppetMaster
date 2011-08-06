20e8c30f330ccedbdc09cd61eab38045c417d845
guojian miguel wu <guojenman@gmail.com>
2011-08-06 00:03:08 -0400

    initial commit

---
+++ Include/WuCinderNITE.h
@@ -0,0 +1,106 @@
+/*
+ * WuCinderNITE.h
+ *
+ *  Created on: Jul 23, 2011
+ *      Author: guojianwu
+ */
+#ifndef WUCINDERNITE_H_
+#define WUCINDERNITE_H_
+
+#pragma once
+
+#include "cinder/Cinder.h"
+#include "cinder/app/AppBasic.h"
+#include "cinder/gl/gl.h"
+
+#include <boost/thread/thread.hpp>
+#include <boost/shared_ptr.hpp>
+
+#include <XnOpenNI.h>
+#include <XnCppWrapper.h>
+#include <XnCodecIDs.h>
+
+using namespace std;
+
+#define MAX_DEPTH 10000
+#define CHECK_RC(status, what, isFatal) \
+if (status != XN_STATUS_OK) \
+{ \
+	ci::app::console() << "failed:" << what << ":" << xnGetStatusString(status) << std::endl; \
+	if(isFatal) { exit(-1); } \
+}
+
+class WuCinderNITE {
+public:
+	static WuCinderNITE* getInstance();
+	virtual ~WuCinderNITE();
+
+	void setup(string xmlpath, XnMapOutputMode mapMode, bool useDepthMap = true, bool useColorImage = true);
+	void setup(string onipath);
+	void startUpdating();
+	void stopUpdating();
+	void shutdown();
+
+	ci::Surface8u getDepthSurface();
+	ci::Surface8u getImageSurface();
+	XnMapOutputMode getMapMode();
+
+	void renderDepthMap(ci::Area area);
+	void renderSkeleton(ci::Area area);
+	void renderLimb(XnUserID player, XnSkeletonJoint eJoint1, XnSkeletonJoint eJoint2, float confidence = 0.5f);
+	void renderColor(ci::Area area);
+	void debugNodeTypes();
+
+	unsigned short		maxDepth;
+	XnMapOutputMode		mMapMode;
+	xn::Context			mContext;
+	xn::DepthGenerator	mDepthGen;
+	xn::UserGenerator	mUserGen;
+	xn::ImageGenerator	mImageGen;
+	xn::SceneAnalyzer	mSceneAnalyzer;
+	xn::SceneMetaData	mSceneMeta;
+	xn::DepthMetaData	mDepthMeta;
+	xn::ImageMetaData	mImageMeta;
+	XnPlane3D			mFloor;
+	XnUserID			mUsers[15];
+	XnUInt16			mNumUsers;
+
+
+protected:
+	WuCinderNITE();
+
+	void update();
+	void updateDepthSurface();
+	void updateImageSurface();
+	void registerCallbacks();
+	void unregisterCallbacks();
+	void findUsers();
+	void assignPlayer(XnUserID nId);
+
+	static WuCinderNITE* mInstance;
+	/**
+	 * Flag for update loop (exits when false)
+	 */
+	volatile bool		mRunUpdates;
+	boost::shared_ptr<boost::thread>	mThread;
+
+	bool				mNeedPoseForCalibration;
+	bool				mIsCalibrated;
+	bool				mUseColorImage;
+	bool				mUseDepthMap;
+	ci::Surface8u		mImageSurface;
+	ci::Area			mDrawArea;
+	ci::Surface8u		mDepthSurface;
+	float 				mDepthHistogram[MAX_DEPTH];
+
+	XnChar				mCalibrationPose[20];
+	XnCallbackHandle	hUserCBs, hCalibrationCompleteCBs, hPoseCBs;
+
+	void static XN_CALLBACK_TYPE CB_NewUser(xn::UserGenerator& generator, XnUserID nId, void* pCookie);
+	void static XN_CALLBACK_TYPE CB_LostUser(xn::UserGenerator& generator, XnUserID nId, void* pCookie);
+	void static XN_CALLBACK_TYPE CB_CalibrationComplete(xn::SkeletonCapability& skeleton, XnUserID nId, XnCalibrationStatus eStatus, void* cxt);
+	void static XN_CALLBACK_TYPE CB_PoseDetected(xn::PoseDetectionCapability& capability, const XnChar* strPose, XnUserID nId, void* pCookie);
+
+};
+
+#endif /* WUCINDERNITE_H_ */
---
+++ Src/PuppetMaster.cpp
@@ -0,0 +1,81 @@
+#include "cinder/app/AppBasic.h"
+#include "cinder/app/MouseEvent.h"
+#include "cinder/Color.h"
+#include "cinder/gl/gl.h"
+#include "cinder/gl/Texture.h"
+#include "cinder/Vector.h"
+#include "cinder/Rand.h"
+#include "cinder/Camera.h"
+
+#include "WuCinderNITE.h"
+#include <btBulletDynamicsCommon.h>
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class PuppetMaster : public AppBasic {
+public:
+	void prepareSettings( AppBasic::Settings *settings );
+	void setup();
+	void update();
+	void draw();
+	void shutdown();
+
+	void keyUp(KeyEvent event);
+
+	WuCinderNITE* ni;
+
+	CameraPersp mCam;
+	Vec3f mCamEye;
+	Vec3f mCamLookAt;
+	Vec4f lightPosition;
+};
+
+void PuppetMaster::prepareSettings( AppBasic::Settings *settings )
+{
+	settings->setWindowSize( 640, 480 );
+}
+
+void PuppetMaster::setup()
+{
+	XnMapOutputMode mapMode;
+	mapMode.nFPS = 30;
+	mapMode.nXRes = 640;
+	mapMode.nYRes = 480;
+
+//	ni = WuCinderNITE::getInstance();
+//	ni->setup("Resources/Sample-User.xml", mapMode, true, true);
+//	ni->startUpdating();
+
+	mCamEye = Vec3f(0, 0, -500.0f);
+	mCamLookAt = Vec3f::zero();
+}
+
+
+void PuppetMaster::update()
+{
+	mCam.setPerspective(60.0f, getWindowAspectRatio(), 1.0f, 1000.0f);
+	mCam.lookAt(mCamEye, mCamLookAt);
+
+}
+
+void PuppetMaster::draw()
+{
+	gl::clear(ColorA(0, 0, 0, 0), true);
+}
+
+void PuppetMaster::shutdown()
+{
+	console() << "quitting..." << std::endl;
+//	ni->shutdown();
+}
+
+void PuppetMaster::keyUp(KeyEvent event)
+{
+	if (event.getChar() == KeyEvent::KEY_q) {
+		quit();
+	}
+}
+
+CINDER_APP_BASIC( PuppetMaster, RendererGl )
